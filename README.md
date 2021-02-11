ef3f807f077b5b2ffba31d8bff47429354ed9c5d
Chung Byung Jae <jbj828@naver.com>
2021-02-11 18:09:52 +0900

    feat: adding structure database

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # memory-leak-detector
+
+## Project Development Phases
+
+1. Structure Database  
+   MLD library will maintain the information about all structures which the application using.
+
+   - MLD library must know the information about all structures being used by the application.
+   - It is the responsibility of the application to tell the MLD library during initialization about all structures it is using. This is called "structure registration"
+   - MLD library will maintain the structure database(preferably a linked-list) to store application structure information.
+   - Key to search in structure database is "name fo structure"
+
+2. Object Database  
+   MLD library will maintain the information about all objects malloc'd by the application.
+
+3. Memory Leak Detection Algorithm  
+   MLD library triggers Memory Leak Detection Algorithm on Object Database to find leaked objects.
---
+++ mld.h
@@ -0,0 +1,78 @@
+#ifndef __MLD__
+#include <assert.h>
+
+#define MAX_STRUCTURE_NAME_SIZE 128
+#define MAX_FIELD_NAME_SIZE 128
+
+// Enumeration for data types
+typedef enum {
+  UINT8,
+  UINT32,
+  INT32,
+  CHAR,
+  OBJ_PTR,
+  FLOAT,
+  DOUBLE,
+  OBJ_STRUCT
+} data_type_t;
+
+#define OFFSETOF(struct_name, fld_name) \
+  (unsigned int)&(((struct_name *)0)->fld_name)
+
+#define FIELD_SIZE(struct_name, fld_name) sizeof(((struct_name *)0)->fld_name)
+
+typedef struct _struct_db_rec_t struct_db_rec_t;
+
+// Structure to store the information of one field of a C structure
+typedef struct _field_info_ {
+  char fname[MAX_FIELD_NAME_SIZE];
+  data_type_t dtype;
+  unsigned int size;
+  unsigned int offset;
+  char nested_str_name[MAX_STRUCTURE_NAME_SIZE];
+} field_info_t;
+
+// Structure to store the information of one C structure
+// which could have 'n_fields' fields
+struct _struct_db_rec_t {
+  struct_db_rec_t *next;
+  char struct_name[MAX_STRUCTURE_NAME_SIZE];  // key
+  unsigned int ds_size;                       // Size of the structure
+  unsigned int n_fields;  // Number of fields in the structure
+  field_info_t *fields;   // pointer to the array of fields
+};
+
+// The head of the linked list representing the structure database
+typedef struct _struct_db_ {
+  struct_db_rec_t *head;
+  unsigned int count;
+} struct_db_t;
+
+// Printing functions
+void print_structure_rec(struct_db_rec_t *struct_rec);
+void print_structure_db(struct_db_t *struct_db);
+
+// function to add the structure record in a structure database
+int add_structure_to_struct_db(struct_db_t *struct_db,
+                               struct_db_rec_t *struct_rec);
+
+// Structure registration helping APIs
+#define FIELD_INFO(struct_name, fld_name, dtype, nested_struct_name) \
+  {                                                                  \
+#fld_name, dtype, FIELD_SIZE(struct_name, fld_name),             \
+        OFFSETOF(struct_name, fld_name), #nested_struct_name         \
+  }
+
+#define REG_STRUCT(struct_db, st_name, fields_arr)                \
+  do {                                                            \
+    struct_db_rec_t *rec = calloc(1, sizeof(struct_db_rec_t));    \
+    strncpy(rec->struct_name, #st_name, MAX_STRUCTURE_NAME_SIZE); \
+    rec->ds_size = sizeof(st_name);                               \
+    rec->n_fields = sizeof(fields_arr) / sizeof(field_info_t);    \
+    rec->fields = fields_arr;                                     \
+    if (add_structure_to_struct_db(struct_db, rec)) {             \
+      assert(0);                                                  \
+    }                                                             \
+  } while (0);
+
+#endif
